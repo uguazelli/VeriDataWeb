eaf8a891f1dd0daa62880682ce8bc39f72144d39
Ugo Guazelli <guazelli.ugo@gmail.com>
2026-01-15 14:21:33 -0300

    feat: Integrate Chatwoot customer support widget.

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,3 +262,19 @@ function translateUI(context) {
 function translateFooter() {
     translateUI('footer');
 }
+
+// Chatwoot Widget Initialization
+window.chatwootSettings = { "position": "right", "type": "expanded_bubble", "launcherTitle": "Chat with us" };
+(function (d, t) {
+    var BASE_URL = "https://chat.veridatapro.com";
+    var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    g.src = BASE_URL + "/packs/js/sdk.js";
+    g.async = true;
+    s.parentNode.insertBefore(g, s);
+    g.onload = function () {
+        window.chatwootSDK.run({
+            websiteToken: 'gpc5EickvYYEuRzfh6w2a6wC',
+            baseUrl: BASE_URL
+        })
+    }
+})(document, "script");
